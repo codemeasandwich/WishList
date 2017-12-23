307f4c9c31ab44b0c3541e9de2b4d68d84fbd82b
brian <brian.shann@gmail.com>
2017-12-23 09:08:04 +0000

    Test mobx-state-tree Models by Recording Snapshots or Patches

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,3 +1,4 @@
+import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree"
 import { WishList, WishListItem } from "./WishList"
 
 it("can create a instance of a model", () =>{
@@ -23,16 +24,43 @@ it("can create a WishList", () =>{
 })
 
 it("can add new items a WishList", () =>{
-  const list = WishList.create()
+  const list = WishList.create();
+  const states = [];
   
-  const item = WishListItem.create({
+  onSnapshot(list, snapshot => {
+    states.push(snapshot)  
+  })
+  
+  const item = {
     "name":"Dog VHS",
     "price":2.45
-  })
+  }
   list.add(item)
   
   expect(list.items.length).toBe(1)
   expect(list.items[0].name).toBe("Dog VHS")
   list.items[0].changeName("The Dog Video")
   expect(list.items[0].name).toBe("The Dog Video")
+  
+  expect(getSnapshot(list)).toMatchSnapshot() 
+  expect(states).toMatchSnapshot() 
+})
+
+it("can add new items a WishList - 2", () =>{
+  const list = WishList.create();
+  const patchs = [];
+  
+  onPatch(list, patch => {
+    patchs.push(patch)  
+  })
+  
+  const item = {
+    "name":"Dog VHS",
+    "price":2.45
+  }
+  list.add(item)
+  
+  list.items[0].changeName("The Dog Video")
+  
+  expect(patchs).toMatchSnapshot() 
 })
