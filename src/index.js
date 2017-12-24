5d419fd2788e4efbafcd5e1a541359eea22bb51a
brian <brian.shann@gmail.com>
2017-12-24 21:07:13 +0000

    Store Store in Local Storage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { WishList } from "./models/WishList"
+import { onSnapshot } from "mobx-state-tree"
 
-const wishList = WishList.create({
+let init = {
 
   items:[{
       name:"Studio Ghibli Howl's Moving Castle All Year Calendar",
@@ -17,12 +18,24 @@ const wishList = WishList.create({
       price:59.99,
       image:"https://images-na.ssl-images-amazon.com/images/I/51K0BQGC29L.jpg"
     }]
-})
+}
+
+if (localStorage.getItem("wishlistapp")) {
+  const json = JSON.parse(localStorage.getItem("wishlistapp"))
+  if (WishList.is(json)) {
+    init = json
+  }
+}
+
+const wishList = WishList.create(init)
 /*
 setInterval(()=>{
   wishList.items[0].changePrice(
   wishList.items[0].price + 1 )
   },1000)
 */
+
+onSnapshot(wishList, snapshot => localStorage.setItem("wishlistapp",JSON.stringify(snapshot)))
+
 ReactDOM.render(<App wishList={wishList}/>, document.getElementById('root'));
 registerServiceWorker();
